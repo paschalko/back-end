cc89911d422af73379a77d10036f44feeee92035
unknown <paschalko>
2024-12-01 22:29:19 +0100

    modified front end

--- express.js
+++ express.js
@@ -211,7 +211,7 @@ app.get('/search', async (req, res) => {
 
 // Handle 404 errors
 app.use((req, res) => {
-    res.status(404).send("Resource not f jkdklmldbdvgsfrsmakound!!");
+    res.status(404).send("Resource not f jkdklml,bjlkjlkik;lm;lldbdvgsfrsmakound!!");
 });
 
 // Function to start the serverbb
